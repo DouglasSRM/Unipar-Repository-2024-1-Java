290d999786abbd6eb3d47ad864cbf48c960c4536
Douglas <138374343+DouglasSRM@users.noreply.github.com>
2024-02-20 22:22:22 -0300

    Create Atividade1-1.java
    Atividade 1-1, Programação Orientada a Objetos
    
    "Crie 10 variáveis de sua escolha, utilize tipos de variáveis diferentes"

---
+++ Atividade1-1.java
@@ -0,0 +1,25 @@
+class Atividade {
+    public static void main(String[] args) {
+        
+        int idade = 19;
+        
+        double peso = 20.54;
+        
+        float nota = 6.8f;
+        
+        long data = 20022024;
+        
+        short ano = 2024;
+        
+        byte unidade = 2;
+        
+        char sexo = 'M';
+        
+        boolean aprovacao = true;
+        
+        int semestre = 2;
+        
+        double altura = 1.75;
+        
+    }
+}
