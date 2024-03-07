ddfb9e6ece2b90c0d2e6b4d7703ce3963d113f9e
Douglas <138374343+DouglasSRM@users.noreply.github.com>
2024-03-06 23:33:47 -0300

    Update Atividade1_4.java

--- Atividade1_4.java
+++ Atividade1_4.java
@@ -35,36 +35,40 @@ public class Atividade1_4 {
         double precoUnitarioProduto;
         int quantidadeEstoque;
 
-        System.out.println("# Cadastro de produtos #");
-        System.out.println("Digite o nome do produto: ");
-        nomeDoProduto = scanner.nextLine();
-        System.out.println("Digite a categoria do produto: ");
-        categoriaDoProduto = scanner.nextLine();
-        System.out.println("Digite o preçõ da unidade do produto: ");
-        while(true) {
-            try {
-                precoUnitarioProduto = scanner.nextDouble();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Por favor, insira um valor válido.");
-                scanner.nextLine();
+        try {
+            System.out.println("# Cadastro de produtos #");
+            System.out.println("Digite o nome do produto: ");
+            nomeDoProduto = scanner.nextLine();
+            System.out.println("Digite a categoria do produto: ");
+            categoriaDoProduto = scanner.nextLine();
+            System.out.println("Digite o preçõ da unidade do produto: ");
+            while (true) {
+                try {
+                    precoUnitarioProduto = scanner.nextDouble();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Por favor, insira um valor válido.");
+                    scanner.nextLine();
+                }
             }
-        }
-        System.out.println("Digite a quantidade em estoque do produto: ");
-        while(true) {
-            try {
-                quantidadeEstoque = scanner.nextInt();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Por favor, insira uma quantidade válida.");
-                scanner.nextLine();
+            System.out.println("Digite a quantidade em estoque do produto: ");
+            while (true) {
+                try {
+                    quantidadeEstoque = scanner.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Por favor, insira uma quantidade válida.");
+                    scanner.nextLine();
+                }
             }
-        }
 
-        System.out.println("Produto #1");
-        System.out.println("Nome: " + nomeDoProduto);
-        System.out.println("Categoria: " + categoriaDoProduto);
-        System.out.println("Preço: " + precoUnitarioProduto);
-        System.out.println("Estoque: " + quantidadeEstoque);
+            System.out.println("Produto #1");
+            System.out.println("Nome: " + nomeDoProduto);
+            System.out.println("Categoria: " + categoriaDoProduto);
+            System.out.println("Preço: " + precoUnitarioProduto);
+            System.out.println("Estoque: " + quantidadeEstoque);
+        } finally {
+            scanner.close();
+        }
     }
 }
