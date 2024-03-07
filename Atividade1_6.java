71ded017714b81998c3fb322437a62caff99ca1c
Douglas <138374343+DouglasSRM@users.noreply.github.com>
2024-03-06 23:18:37 -0300

    Update Atividade1_6.java

--- Atividade1_6.java
+++ Atividade1_6.java
@@ -1,10 +1,31 @@
+/**
+ *   Ajuste o código abaixo
+ *   public class Main {
+ *       public static void main(String[] args) {
+ 
+ *           int numeroProduto = "ABC123"; 
+ *           double precoProduto = 25; 
+ 
+ *           String descricaoProduto;
+ 
+ *           System.out.println("Descrição do Produto: " + descricaoProduto);
+ 
+ *           descricaoProduto = "Fone de ouvido Bluetooth"
+ 
+ *           System.out.println("Dados do Produto:");
+ *           System.out.println("Número: " + numroProduto);
+ *           System.out.println("Preço: $" + precoProduto);
+ *           System.out.println("Descrição: " + descricaoProduto);
+ *       }
+ *   }
+ */
+
 public class Atividade1_6 {
     public static void main(String[] args) {
-
         int numeroProduto = 123;
         double precoProduto = 25;
         String descricaoProduto = "Fone de ouvido Bluetooth";
-        
+
         System.out.println("Dados do Produto:");
         System.out.println("Número: " + numeroProduto);
         System.out.println("Preço: $" + precoProduto);
