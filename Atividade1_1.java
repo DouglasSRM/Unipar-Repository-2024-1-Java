f9420c30e3014a6d8ad7d39d18e1f58015d7ec4b
Douglas <138374343+DouglasSRM@users.noreply.github.com>
2024-03-06 22:51:09 -0300

    Update Atividade1_1.java

--- Atividade1_1.java
+++ Atividade1_1.java
@@ -1,24 +1,16 @@
-class Atividade {
+// "Crie 10 variáveis de sua escolha, utilize tipos de variáveis diferentes."
+
+class Atividade1_1 {
     public static void main(String[] args) {        
         int idade = 19;
-        
         double peso = 20.54;
-        
         float nota = 6.8f;
-        
         long data = 20022024;
-        
         short ano = 2024;
-        
-        byte unidade = 2;
-        
-        char sexo = 'M';
-        
-        boolean aprovacao = true;
-        
-        int semestre = 2;
-        
+        byte unidade = 2;       
+        char sexo = 'M';      
+        boolean aprovacao = true;       
+        int semestre = 2;        
         double altura = 1.75;
-        
     }
 }
