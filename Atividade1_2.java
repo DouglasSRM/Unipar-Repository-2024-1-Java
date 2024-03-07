daca0dded68e9a676e18781cc16ebf060237fe89
Douglas <138374343+DouglasSRM@users.noreply.github.com>
2024-03-06 22:54:28 -0300

    Update Atividade1_2.java

--- Atividade1_2.java
+++ Atividade1_2.java
@@ -1,3 +1,17 @@
+/**
+ *     "Corrija os erros de nomenclatura do padrão camelCase e crie no java."
+ *     a. 1nomeProduto
+ *     b. preçoProduto%
+ *     c. marca do produto
+ *     d. quantidade produto
+ *     e. _descricaoProduto
+ *     f. NomeProduto
+ *     g. 123_numeroProduto
+ *     h. precoDoProduto
+ *     i. estoque Disponivel
+ *     j. totaldeProdutos       
+ */
+
 public class Atividade1_2 {
     public static void main(String[] args) {
         String nomeProduto;
@@ -9,7 +23,6 @@ public class Atividade1_2 {
         int numeroProduto;
         double precoDoProduto;
         int estoqueDisponivel;
-        int totalDeProdutos;
-
+        int totaldeProdutos;
     }
 }
