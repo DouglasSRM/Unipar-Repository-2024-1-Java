dde5852041ce440fd376ce68b2495cd8cf7d2c10
Douglas <138374343+DouglasSRM@users.noreply.github.com>
2024-03-11 22:33:00 -0300

    Create Atividade4_IF03.java

---
+++ Atividade4_IF03.java
@@ -0,0 +1,9 @@
+/**
+ *  3. Verificação de login:
+ *  Crie um programa Java que pede ao usuário para inserir seu nome de usuário e senha.
+ *  Se o nome de usuário for "usuario" e a senha for "senha123", o programa deve imprimir "Login bem-sucedido".
+ *  Caso contrário, deve imprimir "Nome de usuário ou senha incorretos".
+ */
+
+public class Atividade4_IF03 {
+}
